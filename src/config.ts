fbd44f33085cdc65a86e0bafa3f381ac2a359293
bryan-becker-isi <bryan.becker@senairs.org.br>
2025-03-21 08:23:54 -0300

    add class to read env config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export class EnvReader {
+    getRequiredEnvVariable(name: string): string {
+        const value = process.env[name];
+        if (!value) {
+            throw new Error(`Environment variable ${name} is not set.`);
+        }
+        return value;
+    }
+}
