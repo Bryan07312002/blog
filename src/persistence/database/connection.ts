b9bc45fe654e3ad416ddc6f0e2cd0efd3786b463
bryan-becker-isi <bryan.becker@senairs.org.br>
2025-03-21 08:47:28 -0300

    add initial database connections

---
+++ src/persistence/database/connection.ts
@@ -0,0 +1,20 @@
+import { Kysely, PostgresDialect } from "kysely";
+import { Pool } from "pg";
+import { PostgresConfig } from "./config";
+import { Database } from "./types";
+
+export class KyselyDatabaseConnection extends Kysely<Database> {
+    constructor(conf: PostgresConfig) {
+        super({
+            dialect: new PostgresDialect({
+                pool: new Pool({
+                    host: conf.host,
+                    port: conf.port,
+                    user: conf.host,
+                    password: conf.password,
+                    database: conf.database,
+                }),
+            }),
+        });
+    }
+}
---
+++ src/persistence/database/types.ts
@@ -0,0 +1,35 @@
+import { UUID } from "crypto";
+import { UserRole, UserState } from "../../models";
+
+export type Database = {
+    users: UsersTable;
+    profiles: UserProfilesTable;
+    posts: PostsTable;
+};
+
+export interface UsersTable {
+    uuid: UUID;
+    username: string;
+    email: string;
+    password: string;
+    userRole: UserRole[];
+    state: UserState;
+}
+
+export interface UserProfilesTable {
+    userUuid: UUID;
+    title?: string;
+    bio?: string;
+    profile_photo_url?: string;
+    lives_in?: string;
+    works_at?: string;
+    studie_at?: string;
+}
+
+export interface PostsTable {
+    uuid: UUID;
+    title: string;
+    user_id: UUID;
+    posted_at: string;
+    file_url: string;
+}
