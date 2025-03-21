2363864e79ce40682ab4c518e5f3ceeadb880abb
bryan-becker-isi <bryan.becker@senairs.org.br>
2025-03-21 16:33:07 -0300

    app has factories now

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import { PostgresConfig } from "./persistence/database/config";
 import { KyselyDatabaseConnection } from "./persistence/database/connection";
 import { FastifyServer } from "./server/fastify_server";
+import { RegisterController } from "./controllers/register";
+import {
+    ServiceFactory,
+    ValidatorFactory,
+    RepositoryFactory,
+    UUIDGeneratorFactory,
+} from "./controllers/services_factory";
 
 export class App {
     constructor(
         public readonly databaseConnection: KyselyDatabaseConnection,
         public readonly server: FastifyServer,
+        public serviceFactory: ServiceFactory,
     ) {}
 
     static initialize(): App {
@@ -13,7 +21,17 @@ export class App {
         const db = new KyselyDatabaseConnection(postgresConfig);
 
         const server = new FastifyServer();
-        const app = new App(db, server);
+
+        const repositoryFactory = new RepositoryFactory(db, { salt: 10 });
+        const validatorFactory = new ValidatorFactory();
+        const uuiDGeneratorFactory = new UUIDGeneratorFactory();
+        const serviceFactory = new ServiceFactory(
+            repositoryFactory,
+            validatorFactory,
+            uuiDGeneratorFactory,
+        );
+
+        const app = new App(db, server, serviceFactory);
         app.registerRoutes();
 
         return app;
@@ -24,6 +42,12 @@ export class App {
             res.writeHead(200, { "content-type": "application/html" });
             res.end(JSON.stringify({ status: "Ok" }));
         });
+
+        this.server.post(
+            "/register",
+            [],
+            new RegisterController(this.serviceFactory).handler,
+        );
     }
 
     run() {
